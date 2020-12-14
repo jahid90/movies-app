24e11fd55a44b40d56238f2a678c0652bc5de2af
Jahid <s.jahid.90@gmail.com>
2020-12-14 17:46:50 +0530

    [service] init spring boot service

---
+++ src/test/java/io/jahiduls/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jahiduls.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
