8209dcd7b4ccbe1c3d2cde25904b4b0e8a61c675
Jahid <s.jahid.90@gmail.com>
2020-12-18 23:36:48 +0530

    [service] discovery: add capability to query for a specific page of results

--- src/main/java/io/jahiduls/movies/services/tmdb/MoviesDiscovery.java
+++ src/main/java/io/jahiduls/movies/services/tmdb/MoviesDiscovery.java
@@ -1,39 +1,26 @@
 package io.jahiduls.movies.services.tmdb;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import io.jahiduls.movies.client.HttpClientFactory;
-import io.jahiduls.movies.configuration.TmdbConfiguration;
 import io.jahiduls.movies.services.model.Movie;
 import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
 import reactor.core.publisher.Mono;
 
 public class MoviesDiscovery {
 
     @ToString
-    @Component
     @RequiredArgsConstructor
     public static class Request {
 
-        private final TmdbConfiguration tmdbConfiguration;
-        private final HttpClientFactory clientFactory;
-
-        private static final String BASE_URL = "https://api.themoviedb.org/3";
-        private static final String DISCOVERY_PATH = "/discover/movie";
+        private final RequestHeadersSpec<?> request;
 
         public Mono<Response> execute() {
 
-            final WebClient webClient = clientFactory.webClientOf(BASE_URL + DISCOVERY_PATH);
-
-            return webClient.get()
-                    .uri(uriBuilder -> uriBuilder.queryParam("api_key", tmdbConfiguration.token()).build())
-                    .retrieve()
+            return request.retrieve()
                     .bodyToMono(Response.class);
         }
     }
